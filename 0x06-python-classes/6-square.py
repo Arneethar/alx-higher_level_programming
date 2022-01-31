05e97a4dbd5e964300f63df454c7074afcfb5ac2
Arneethar <anitamatthewc1@gmail.com>
2022-01-31 05:46:16 -0800

    test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 """
-Private instance attribute: size:
-  property def size(self): to retrieve it
-  property setter def size(self, value): to set it:
-  size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
-Private instance attribute: position:
-  property def position(self): to retrieve it
-  property setter def position(self, value): to set it:
-  position must be a tuple of 2 positive integers, otherwise raise a TypeError exception with the message position must be a tuple of 2 positive integers
-Instantiation with optional size and optional position: def __init__(self, size=0, position=(0, 0)):
-Public instance method: def area(self): that returns the current square area
-Public instance method: def my_print(self): that prints in stdout the square with the character #:
-if size is equal to 0, print an empty line
-position should be use by using space - Don’t fill lines by spaces when position[1] > 0
+Defining a square class
 """
 class Square:
   """This class creates a square"""
@@ -42,12 +29,10 @@ class Square:
 
   @position.setter
   def position(self,value):
-    if ((not isinstance(value,tuple)) and len(value) < 2):
-        if value[0] < 0 and value[1] < 0:
-          raise TypeError("position must be a tuple of 2 positive integers")
+    if (not isinstance(value,tuple) or len(value) != 2) or not all(isinstance(num, int) for num in value) or not all(num >= 0 for num in value):
+      raise TypeError("position must be a tuple of 2 positive integers")
     self.__position = value
-
-
+          
   def area(self):
     """Returns the area of self"""
     return self.__size ** 2
