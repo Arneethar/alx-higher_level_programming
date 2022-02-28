ab55a735c8cff1aa9d8c00819e49aa25a7b7eeeb
Arneethar <anitamatthewc1@gmail.com>
2022-02-28 13:25:54 -0800

    Using process and argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let count = process.argv.length - 1;
+if(count === 1){
+    console.log('No argument');
+}
+else if(count === 2){
+    console.log('Argument found');
+}
+else
+{console.log('Arguments found');}
